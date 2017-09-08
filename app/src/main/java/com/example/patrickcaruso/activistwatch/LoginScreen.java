fb7c33d12b261caf8ae48b9a45359cfe0442ff63
patrickcaruso <patrickec246@gmail.com>
2017-09-08 14:01:14 -0400

    Basic login screen & start activity

--- app/src/main/java/com/example/patrickcaruso/activistwatch/LoginScreen.java
+++ app/src/main/java/com/example/patrickcaruso/activistwatch/LoginScreen.java
@@ -1,9 +1,11 @@
 package com.example.patrickcaruso.activistwatch;
 
-import android.content.Context;
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -11,13 +13,24 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Context context = getApplicationContext();
-        CharSequence text = "Hello toast 1!!";
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-
         setContentView(R.layout.activity_login_screen);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
