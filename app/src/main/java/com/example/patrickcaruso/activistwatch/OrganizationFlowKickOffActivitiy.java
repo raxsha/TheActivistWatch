0549850e59dc860664bbfa723da17ae4b128b33b
Stephen <scha9393@gmail.com>
2017-09-08 12:41:27 -0400

    Committed Activities

---
+++ app/src/main/java/com/example/patrickcaruso/activistwatch/OrganizationFlowKickOffActivitiy.java
@@ -0,0 +1,13 @@
+package com.example.patrickcaruso.activistwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrganizationFlowKickOffActivitiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organization_flow_kick_off_activitiy);
+    }
+}
