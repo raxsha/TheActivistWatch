63e99e8b2d4492a510faba33a679323d9d29033c
Stephen <scha9393@gmail.com>
2017-09-08 13:36:06 -0400

    ..

--- app/src/main/java/com/example/patrickcaruso/activistwatch/OrganizationFlowKickOffActivity.java
+++ app/src/main/java/com/example/patrickcaruso/activistwatch/OrganizationFlowKickOffActivity.java
@@ -2,6 +2,8 @@ package com.example.patrickcaruso.activistwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OrganizationFlowKickOffActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class OrganizationFlowKickOffActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organization_flow_kick_off_activity);
+
+        Button yes_button = (Button) findViewById(R.id.YesOrganizationKickOffButton);
+        //Button no_button = (Button) findViewById(R.id.NoOrganizationKickOffButton);
+
+        yes_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_create_organization);
+            }
+        });
     }
 }
