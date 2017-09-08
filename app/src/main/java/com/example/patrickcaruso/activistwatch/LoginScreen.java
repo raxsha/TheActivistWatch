103d3c814954f52369aa2f2c52d364252492a8cf
patrickcaruso <patrickec246@gmail.com>
2017-09-08 12:26:07 -0400

    Test push

--- app/src/main/java/com/example/patrickcaruso/activistwatch/LoginScreen.java
+++ app/src/main/java/com/example/patrickcaruso/activistwatch/LoginScreen.java
@@ -1,22 +1,13 @@
 package com.example.patrickcaruso.activistwatch;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class LoginScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Context context = getApplicationContext();
-        CharSequence text = "Hello toast1";
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
 
         setContentView(R.layout.activity_login_screen);
     }
