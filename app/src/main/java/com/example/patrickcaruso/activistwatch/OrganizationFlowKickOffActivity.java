eee742ac44d60964f97f387f7d487616edae4fe2
Stephen <scha9393@gmail.com>
2017-09-08 13:09:08 -0400

    ..

--- app/src/main/java/com/example/patrickcaruso/activistwatch/OrganizationFlowKickOffActivity.java
+++ app/src/main/java/com/example/patrickcaruso/activistwatch/OrganizationFlowKickOffActivity.java
@@ -3,11 +3,11 @@ package com.example.patrickcaruso.activistwatch;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class OrganizationFlowKickOffActivitiy extends AppCompatActivity {
+public class OrganizationFlowKickOffActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_organization_flow_kick_off_activitiy);
+        setContentView(R.layout.activity_organization_flow_kick_off_activity);
     }
 }
