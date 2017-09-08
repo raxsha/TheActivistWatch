e71d8ae23d75372ab6ccfb4c664d79c639c9246b
Greenness <patrickblack154@gmail.com>
2017-09-08 18:55:08 -0400

    Edited org creation file

--- app/src/main/java/com/example/patrickcaruso/activistwatch/CreateOrganizationActivity.java
+++ app/src/main/java/com/example/patrickcaruso/activistwatch/CreateOrganizationActivity.java
@@ -2,6 +2,7 @@ package com.example.patrickcaruso.activistwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class CreateOrganizationActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class CreateOrganizationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_organization);
+
+        EditText editOrg =  (EditText) findViewById(R.id.orgName);
+        EditText editDes =  (EditText) findViewById(R.id.description);
     }
 }
