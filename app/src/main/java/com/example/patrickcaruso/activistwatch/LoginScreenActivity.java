19bfd3da6f1f8c194624932f32086e505f1eac66
patrickcaruso <patrickec246@gmail.com>
2017-09-09 14:21:36 -0400

    Prettify Login screen yet more

--- app/src/main/java/com/example/patrickcaruso/activistwatch/LoginScreenActivity.java
+++ app/src/main/java/com/example/patrickcaruso/activistwatch/LoginScreenActivity.java
@@ -28,7 +28,7 @@ public class LoginScreenActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText usernameTextArea = (EditText) findViewById(R.id.orgName);
+                EditText usernameTextArea = (EditText) findViewById(R.id.loginUsername);
                 EditText passwordTextArea = (EditText) findViewById(R.id.loginPassword);
 
                 String username = usernameTextArea.getText().toString();
