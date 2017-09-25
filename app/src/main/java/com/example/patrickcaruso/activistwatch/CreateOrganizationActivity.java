16adcdbe075f6269543466b11cd7ea40288f2e42
Greenness <patrickblack154@gmail.com>
2017-09-25 14:47:28 -0400

    Added organization class Edited CreateOrgActivity and Dashboard activity/xml

--- app/src/main/java/com/example/patrickcaruso/activistwatch/CreateOrganizationActivity.java
+++ app/src/main/java/com/example/patrickcaruso/activistwatch/CreateOrganizationActivity.java
@@ -1,11 +1,17 @@
 package com.example.patrickcaruso.activistwatch;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.patrickcaruso.activistwatch.Database.Database;
+
+import java.io.IOException;
 
 public class CreateOrganizationActivity extends AppCompatActivity {
 
@@ -14,22 +20,45 @@ public class CreateOrganizationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_organization);
 
-        EditText editOrg =  (EditText) findViewById(R.id.orgName);
-        EditText editDes =  (EditText) findViewById(R.id.description);
 
-        String orgName = editOrg.getText().toString();
-        String orgDes = editDes.getText().toString();
 
         Button enterButton = (Button) findViewById(R.id.enterButton);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editOrg =  (EditText) findViewById(R.id.orgName);
+                EditText editDes =  (EditText) findViewById(R.id.description);
 
-        if (!orgName.isEmpty() && !orgDes.isEmpty()) {
-            enterButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(getApplicationContext(), CreateEventActivity.class);
-                    startActivity(intent);
+                String orgName = editOrg.getText().toString();
+                String orgDes = editDes.getText().toString();
+
+                try {
+                    //not done yet
+                    int createOrgResponse = Database.createOrganization(1, "name", "", "","");
+                    if (createOrgResponse > 0) {
+
+                        //Go to Dashboard
+                        Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
+                        startActivity(intent);
+                    } else {
+                        displayCreateOrgErrorMessage(orgName, orgDes);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            });
-        }
+            }
+        });
+    }
+
+    /**
+     * Displays the org name error message
+     * @param orgName
+     * @param description
+     */
+    private void displayCreateOrgErrorMessage(String orgName,
+                                          String description) {
+        Context context = getApplicationContext();
+        CharSequence text = "Left Organization name and/or description blank";
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/patrickcaruso/activistwatch/DashboardActivity.java
+++ app/src/main/java/com/example/patrickcaruso/activistwatch/DashboardActivity.java
@@ -1,7 +1,10 @@
 package com.example.patrickcaruso.activistwatch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Button createOrgButton = (Button) findViewById(R.id.createOrgButton);
+        createOrgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CreateOrganizationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
