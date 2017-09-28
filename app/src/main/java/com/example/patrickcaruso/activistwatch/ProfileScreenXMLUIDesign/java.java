ab3dfe0d95ed691a330f77c980826bf5c8e30e37
Raksha Muthukumar <raksha.muth@gatech.edu>
2017-09-28 18:16:08 -0400

    Edit profile activity design ready.

---
+++ app/src/main/java/com/example/patrickcaruso/activistwatch/ProfileScreenXMLUIDesign/java.java
@@ -0,0 +1,14 @@
+package viralandroid.com.androidmaterialdesigntutorial;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class ProfileScreenXMLUIDesign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.material_design_profile_screen_xml_ui_design);
+    }
+}
