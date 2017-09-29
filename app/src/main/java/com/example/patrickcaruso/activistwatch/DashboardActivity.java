3220a80bfaa7185adddd1e4249635a47370e0e3c
Ju-Hwan <jukim92@gmail.com>
2017-09-28 20:46:55 -0400

    Sidebar tab button operational

--- app/src/main/java/com/example/patrickcaruso/activistwatch/DashboardActivity.java
+++ app/src/main/java/com/example/patrickcaruso/activistwatch/DashboardActivity.java
@@ -1,13 +1,17 @@
 package com.example.patrickcaruso.activistwatch;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class DashboardActivity extends AppCompatActivity {
-
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,8 +19,20 @@ public class DashboardActivity extends AppCompatActivity {
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-    }
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        return mToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
